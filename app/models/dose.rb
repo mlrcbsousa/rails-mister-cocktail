14e6cba3b73f7afc63d9a863bcda7b94384faca2
Manuel Sousa <mlrcbsousa@gmail.com>
2018-11-29 12:31:20 +0000

    schema done with validations and associations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
+  validates :description, presence: true
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
