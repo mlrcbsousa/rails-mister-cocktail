6fef6a93ba0f03fa19420da613c0351563b213cf
Manuel Sousa <mlrcbsousa@gmail.com>
2018-11-29 15:02:44 +0000

    testing new dose creation

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,11 +5,10 @@ class DosesController < ApplicationController
   end
 
   def create
-    @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+
     if @dose.save
-      @dose = Dose.create(dose_params)
-      @dose.cocktail = @cocktail
       @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -25,6 +24,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient)
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,12 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses
+
+  before_destroy :check_for_dose_children
+
+  private
+
+  def check_for_dose_children
+    doses
+  end
 end
