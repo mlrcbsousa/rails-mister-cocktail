48fc489e2ab3b3d2f65aa48823ea28415a6e380c
Manuel Sousa <mlrcbsousa@gmail.com>
2018-12-02 15:30:11 +0000

    beautiful css now

--- sandbox.rb
+++ sandbox.rb
@@ -1,2 +1,8 @@
-Cloudinary::Uploader.upload("logo-mister-cocktail.png",
-  :folder => "lewagon/mistercocktails/logo/", :public_id => "logo-mister-cocktail", :overwrite => true)
+# Cloudinary::Uploader.upload("logo-mister-cocktail.png",
+#   :folder => "lewagon/mistercocktails/logo/", :public_id => "logo-mister-cocktail", :overwrite => true)
+
+
+@reviews = cocktail.reviews
+
+reviews = cocktail.reviews
+reviews.inject(0) { |sum, n| sum + n.rating } / reviews.count
